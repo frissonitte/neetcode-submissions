cc3be244ba5ba42c88418c3dd2bf9f18d0c42c70
Emirhan YILDIRIM <frissonitte619@gmail.com>
2026-04-23 15:56:15 +0300

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        a = Counter(s)
+        b = Counter(t)
+    
+        return a==b
+             
